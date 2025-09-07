6978005e2c302cce568ec94ca88c651b4f2130de
Sreeram5 <spotnuru@stu.naperville203.org>
2025-09-07 08:57:21 -0500

    finished Sieve of Eratosthenes project

--- Chapter 15 Activities/Sieve/src/Sieve.java
+++ Chapter 15 Activities/Sieve/src/Sieve.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.*;
 
 
 /**
@@ -13,13 +14,21 @@ public class Sieve
         int n = in.nextInt();
 
         // Your work goes here
-        . . .
+        Set<Integer> pnums = new HashSet<>();
+        for (int i = 2; i < n; i++){
+            pnums.add(i);
+        }
 
-
-
-
-
-
-
+        Set<Integer> toRemove = new HashSet<>();
+        for(int d = 2; d <= (n/2); d++){
+            for (int num: pnums){
+                if(num%d == 0 && num != d)
+                    toRemove.add(num);
+            }
+        }
+        pnums.removeAll(toRemove);
+        System.out.println("The prime numbers up to " + n + " are:");
+        for(int num: pnums)
+            System.out.print(" " + num);
     }
 }
