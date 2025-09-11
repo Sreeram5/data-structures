d5516d1a20ddc34a44f3d90da4ecd6427d7b7875
Sreeram5 <spotnuru@stu.naperville203.org>
2025-09-11 13:03:19 -0500

    finsihed gradebook prog

--- Chapter 15 Activities/Gradebook/src/Gradebook.java
+++ Chapter 15 Activities/Gradebook/src/Gradebook.java
@@ -1,5 +1,4 @@
-import java.util.Scanner;
-. . .
+import java.util.*;
 /**
  * A program to add, remove, modify or print
  * student names and grades.
@@ -10,8 +9,7 @@ public class Gradebook
     {
         Scanner in = new Scanner(System.in);
 
-        . . .
-
+        Map<String, String> grades = new HashMap<>();
         boolean done = false;
         while(!done)
         {
@@ -22,17 +20,42 @@ public class Gradebook
                 done = true;
             } else if (input.equals("A"))
             {
-                . . .
+                System.out.println("Enter the student's name: ");
+                String name = in.next();
+                System.out.println("Enter the student's grade: ");
+                String grade = in.next();
+                grades.put(name, grade);
+                System.out.println(name + " has been added.");
 
             } else if (input.equals("R"))
             {
-                . . .
+                System.out.println("Enter the student's name: ");
+                String name = in.next();
+                if (grades.containsKey(name))
+                {
+                    grades.remove(name);
+                    System.out.println(name + " has been removed.");
+                } else
+                    System.out.println(name + " is not in the gradebook.");
+
             } else if (input.equals("M"))
             {
-                . . .
+                System.out.println("Enter the student's name: ");
+                String name = in.next();
+                if (grades.containsKey(name))
+                {
+                    System.out.println("Enter the student's new grade: ");
+                    String grade = in.next();
+                    grades.put(name, grade);
+                    System.out.println(name + "'s grade has been updated.");
+                } else
+                    System.out.println(name + " is not in the gradebook.");
+
             } else if (input.equalsIgnoreCase("P"))
             {
-                . . .
+                Set<String> keys = grades.keySet();
+                for (String key: keys)
+                    System.out.println(key + ": " + grades.get(key));
             } else
             {
                 done = true;
