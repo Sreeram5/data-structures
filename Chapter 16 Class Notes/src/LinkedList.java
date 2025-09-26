84667876a0e3f38ba41fdb0d94278b43d9708d80
Sreeram Potnuru <SPotnuru@stu.naperville203.org>
2025-09-26 13:17:29 -0500

    finsihed linkedlist

--- Chapter 16 Class Notes/src/LinkedList.java
+++ Chapter 16 Class Notes/src/LinkedList.java
@@ -8,28 +8,47 @@ import java.util.NoSuchElementException;
 */
 public class LinkedList
 {
-
+    // first refers to the first node in the list
+    // if the list is empty, first will be null
+    private Node first;
 
     /**
         Constructs an empty linked list.
     */
-
-
-
+    public LinkedList()
+    {
+        this.first = null;
+    }
 
     /**
         Returns the first element in the linked list.
         @return the first element in the linked list
     */
 
-
+    public Object getFirst()
+    {
+        if (this.first == null)
+        {
+            throw new NoSuchElementException();
+        }
+        return this.first.data;
+    }
 
 
     /**
         Removes the first element in the linked list.
         @return the removed element
     */
-
+    public Object removeFirst()
+    {
+        if(this.first == null)
+        {
+            throw new NoSuchElementException();
+        }
+        Object element = this.first.data;
+        this.first = this.first.next;
+        return element;
+    }
 
 
 
@@ -38,7 +57,13 @@ public class LinkedList
         Adds an element to the front of the linked list.
         @param element the element to add
     */
-
+    public void addFirst(Object element)
+    {
+        Node node = new Node();
+        node.data = element;
+        node.next = this.first;
+        this.first = node;
+    }
 
 
 
@@ -47,30 +72,80 @@ public class LinkedList
         Returns an iterator for iterating through this list.
         @return an iterator for iterating through this list
     */
+    public LinkedListIterator listIterator()
+    {
+        return new LinkedListIterator();
+    }
 
-
+    public String toString()
+    {
+        ListIterator ListIterator = listIterator();
+        String elements = "[";
+        while (ListIterator.hasNext())
+        {
+            elements += ListIterator.next();
+            if (ListIterator.hasNext())
+            {
+                elements += ", ";
+            }
+        }
+        return elements + "]";
+    }
 
 
 
     //Class Node
+    // Node is a static class because it does not need to access anything in LinkedList
+    //THe object will store information, not interact
+    static class Node
+    {
+        public Node next;
+        public Object data;
+    }
 
 
-    class LinkedListIterator //implements ListIterator
+    class LinkedListIterator implements ListIterator
     {
       //private data
-
+        private Node position;
+        private Node previous;
+        private boolean isAfterNext;
 
         /**
             Constructs an iterator that points to the front
             of the linked list.
         */
-
+        public LinkedListIterator()
+        {
+            this.position = null;
+            this.previous = null;
+            this.isAfterNext = false;
+        }
 
         /**
             Moves the iterator past the next element.
             @return the traversed element
         */
+        public Object next()
+        {
+            if (!hasNext())
+            {
+                throw new NoSuchElementException();
+            }
+            previous =position;
 
+            if (position == null)
+            {
+                position = first;
+            }
+            else
+            {
+                position = position.next;
+            }
+
+            isAfterNext = true;
+            return position.data;
+        }
 
 
 
@@ -79,14 +154,42 @@ public class LinkedList
             Tests if there is an element after the iterator position.
             @return true if there is an element after the iterator position
         */
-
+        public boolean hasNext()
+        {
+            //check if the list is empty
+            if (position == null)
+                return first !=null;
+            
+            //The iterator has moved so check the next node
+            return position.next != null;
+        }
 
         /**
             Adds an element before the iterator position
             and moves the iterator past the inserted element.
             @param element the element to add
         */
+        public void add(Object element)
+        {
+            //Check if the iterator is at the beginning of the list
+            if (position == null)
+            {
+                addFirst(element);
+                position = first;
+            }
 
+            else
+            {
+                Node newNode = new Node();
+                newNode.data = element;
+                newNode.next = position.next;
+                // Set the next element of the CURRENT position to point to our new node
+                position.next = newNode;
+                position = newNode;
+            }
+
+            isAfterNext = false;
+        }
 
 
 
@@ -96,7 +199,27 @@ public class LinkedList
             Removes the last traversed element. This method may
             only be called after a call to the next() method.
         */
+        public void remove()
+        {
+            if(!isAfterNext)
+            {
+                throw new IllegalStateException();
+            }
+            
+            // Check if the iterator is at the beginning
+            if (position == first)
+            {
+                removeFirst();
+                position = null;
+            }
+            else
+            {
+                previous.next = position.next;
+                position = previous;
+            }
 
+            isAfterNext = false;
+        }
 
 
 
@@ -107,6 +230,15 @@ public class LinkedList
             Sets the last traversed element to a different value.
             @param element the element to set
         */
+        public void set(Object element)
+        {
+            if (!isAfterNext)
+            {
+                throw new IllegalStateException();
+            }
+            position.data = element;
+            // We don't have to reset isAfterNext because the structure of the list has not changed
+        }
 
 
 
